d80296d351cf416f73eede0ebf8a7a4c2ea50452
Alan Betanov <a.betanov@kassir.ru>
2024-05-15 10:45:21 +0300

    vk bridge test

--- app.vue
+++ app.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+	<div>
+    Тест VK Bridge
+		<NuxtWelcome />
+	</div>
 </template>
+
+<script>
+import bridge from "@vkontakte/vk-bridge"
+
+console.log("Перед VKWebAppInit")
+
+// Отправляет событие инициализации нативному клиенту
+bridge.send("VKWebAppInit")
+</script>
+
+<style></style>
